f3e7a080a63a5a9edcb3d3c12c59227ff0fb9b9c
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-03 00:51:21 +0530

    user controller file, api util, middlewere files initialized

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+const registerUser = asyncHandler( async (req, res, next) => {
+    res.status(200).json({message : "OK"});
+});
+
+export {registerUser};
